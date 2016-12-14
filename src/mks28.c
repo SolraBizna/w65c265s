9e377126821fe238789dcf02c7846fbd523044d1
Solra Bizna <solra@bizna.name>
2016-12-14 13:48:12 -0700

    Better wayof dealing with the buggy ROM

--- src/mks28.c
+++ src/mks28.c
@@ -8,7 +8,9 @@ int main(int argc, char* argv[]) {
   uint8_t rec[PAGE_SIZE];
   uint32_t cur_addr = 0;
   size_t red;
-  int recordno = 0;
+  // monitor ROM ignores first record, assumes it's an S0 record
+  // let's give it what it wants
+  printf("S0030000FC\n");
   while((red = fread(rec, 1, PAGE_SIZE, stdin)) >= 1) {
     int n;
     for(n = red; n < PAGE_SIZE; ++n) {
@@ -18,20 +20,17 @@ int main(int argc, char* argv[]) {
       if(rec[n] != SENTINEL_BYTE) break;
     }
     if(n < PAGE_SIZE) {
-      do {
-        /* this is a valid block */
-        uint16_t running_sum = (PAGE_SIZE+4)
-          + ((cur_addr>>16)&0xFF)
-          + ((cur_addr>>8)&0xFF)
-          + (cur_addr&0xFF);
-        printf("S2%02X%06X", PAGE_SIZE+4, cur_addr);
-        for(n = 0; n < PAGE_SIZE; ++n) {
-          running_sum += rec[n];
-          printf("%02X", rec[n]);
-        }
-        printf("%02X\n", running_sum&0xFF^0xFF);
-        // bug in the monitor ROM, first record gets ignored
-      } while(recordno++ == 0);
+      /* this is a valid block */
+      uint16_t running_sum = (PAGE_SIZE+4)
+        + ((cur_addr>>16)&0xFF)
+        + ((cur_addr>>8)&0xFF)
+        + (cur_addr&0xFF);
+      printf("S2%02X%06X", PAGE_SIZE+4, cur_addr);
+      for(n = 0; n < PAGE_SIZE; ++n) {
+        running_sum += rec[n];
+        printf("%02X", rec[n]);
+      }
+      printf("%02X\n", running_sum&0xFF^0xFF);
     }
     else {
       /* skip this block */
